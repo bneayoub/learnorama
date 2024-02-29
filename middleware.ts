0365bbf27fb4d46f73d96c00d3a2f9940a849a97
bneayoub <bne.ayoub@gmail.com>
2024-02-29 23:24:38 +0100

    Update ignoredRoutes in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export default authMiddleware({
   publicRoutes: ['/anyone-can-visit-this-route'],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  ignoredRoutes: ['/'],
 });
 
 export const config = {
