e0c57c05eb873888f779f57de804aca1928c86f6
bneayoub <bne.ayoub@gmail.com>
2024-03-03 23:23:29 +0100

    Add DashboardLayout component

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+import { Sidebar } from "./_components/sidebar";
+
+const DashboardLayout = ({
+	children
+
+}: {
+	children: React.ReactNode,
+}) => {
+	return (
+		<div className="h-full">
+			<div className="hidden md:flex h-full w-56 flex-col insert-y-0 z-50">
+				<Sidebar />
+			</div>
+			<main className="md:pl-56 h-full">
+			{children}
+			</main>
+
+		</div>
+	 );
+}
+
+export default DashboardLayout;
