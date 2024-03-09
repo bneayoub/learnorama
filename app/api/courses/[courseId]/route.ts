c9cdefdf664ee83fab87735dd2e6816c02e91d1e
bneayoub <bne.ayoub@gmail.com>
2024-03-09 21:42:14 +0100

    Refactor course update route

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -2,22 +2,29 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
-export async function POST(
+
+
+export async function PATCH(
   req: Request,
+  { params }: { params: { courseId: string}}
 ) {
   try {
     const { userId } = auth();
-    const { title } = await req.json();
+    const { courseId } = params;
+	const values = await req.json();
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const course = await db.course.create({
-      data: {
-        userId,
-        title,
-      }
+    const course = await db.course.update({
+      where: {
+		id: params.courseId,
+		userId
+	  },
+	  data: {
+		...values,
+	  }
     });
 
     return NextResponse.json(course);
