88a47e4a6e6083cc33e145b79690a6e38618407a
bneayoub <bne.ayoub@gmail.com>
2024-03-09 04:17:42 +0100

    Refactor CourseIdPage component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,12 @@
-const CourseIdPage = ({
+import { db } from "@/lib/db";
+
+const CourseIdPage = async ({
 	params
 }:{
 	params : { courseId: string}
 }) => {
+
+
 	return (
 		<div>
 			<h1>Course {params.courseId}</h1>
