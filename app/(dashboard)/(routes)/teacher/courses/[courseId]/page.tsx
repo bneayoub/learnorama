77d6444def8f4370169933ba35bba23d21e06829
bneayoub <bne.ayoub@gmail.com>
2024-03-09 21:34:35 +0100

    Add DescriptionForm component and related files

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/descripton-form";
 
 const CourseIdPage = async ({
 	params
@@ -60,6 +61,10 @@ const CourseIdPage = async ({
 						</h2>
 					</div>
 					<TitleForm
+						initialData ={course}
+						courseId={course.id}
+					/>
+					<DescriptionForm
 						initialData ={course}
 						courseId={course.id}
 					/>
