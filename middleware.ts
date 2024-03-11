aece3977d23314b4ec0afac9a6f2116c336de72c
bneayoub <bne.ayoub@gmail.com>
2024-03-11 02:42:37 +0100

    Update authMiddleware configuration in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,12 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/anyone-can-visit-this-route'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: [''],
+  publicRoutes: ["/api/webhook"]
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
